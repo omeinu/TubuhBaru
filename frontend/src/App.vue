b0312f9df95d13c601509f535bc7467d32a3e312
hiromi <86691510+omeinu@users.noreply.github.com>
2025-06-14 16:22:07 +0900

    Add weight history API and chart component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>TubuhBaru</h1>
   <WeightInput />
+  <WeightChart />
   <MealInput />
   <MealList />
 </template>
@@ -9,5 +10,6 @@
 import MealInput from './components/MealInput.vue'
 import MealList from './components/MealList.vue'
 import WeightInput from './components/WeightInput.vue'
+import WeightChart from './components/WeightChart.vue'
 </script>
 
